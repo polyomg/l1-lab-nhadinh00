9698d38e8f18cce0c3b7e75eed0af95d68789ad1
unknown <nguyencatnhadinhh@gmail.com>
2025-10-06 20:56:09 +0700

    Lab1, Lab2, Lab3_NguyenCatNhaDinh_TV00081_SD20205

---
+++ Lab2/src/main/java/poly/edu/entity/Product.java
@@ -0,0 +1,31 @@
+package poly.edu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    String name;
+    Double price;
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+	public Product(String string, double d) {
+		// TODO Auto-generated constructor stub
+	}
+	public Object getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setPrice(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+}
